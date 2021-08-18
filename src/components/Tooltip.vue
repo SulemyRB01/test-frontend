88aba9b17f5562ad3e842682912b151956880183
SulemyRB01 <surodriguez408@gmail.com>
2021-08-18 16:30:02 -0500

    Se mejora la vista de usuarios con los modales y se agrega el tooltip y se agregan correctamente los script en el html

---
+++ src/components/Tooltip.vue
@@ -0,0 +1,27 @@
+<template>
+  <a href="" data-bs-toggle="tooltip" data-bs-placement="top" :title="textTooltip">
+    <i :class="classIcon"></i>
+  </a>
+</template>
+
+<script>
+import { Tooltip } from 'bootstrap/dist/js/bootstrap.esm.min.js'
+
+export default {
+  name: 'Tooltip',
+  props: {
+    textTooltip: {
+      type: String,
+      default: ''
+    },
+    classIcon: {
+      type: String,
+      default: 'bi bi-file-earmark-arrow-down-fill text-white'
+    }
+  },
+  mounted() {
+    let tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    tooltipTriggerList.forEach(tooltipNode => new Tooltip(tooltipNode))
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap'
 import './styles/app.scss'
 import VueClickAway from "vue3-click-away"
 
